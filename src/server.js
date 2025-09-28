4252ce2f4135cfa7ce4f966308c2584d350ec54c
HryhoriyK <k.hryhoriy88@gmail.com>
2025-09-28 20:48:19 +0200

    hw3-crud

--- src/server.js
+++ src/server.js
@@ -2,12 +2,20 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = Number(getEnvVar('PORT', '8000'));
 
 export const createServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }));
   app.use(cors());
   app.use(
     pino({
@@ -17,36 +25,13 @@ export const createServer = () => {
     })
   );
 
-  app.get('/contacts', async (req, res, next) => {
-    try {
-      const contacts = await getAllContacts();
-      res.json({ status: 200, message: 'Successfully found contacts!', data: contacts });
-    } catch (err) {
-      next(err);
-    }
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts/:id', async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const contact = await getContactById(id);
-      if (!contact) {
-        return res.status(404).json({ status: 404, message: `Contact with ID ${id} not found` });
-      }
-      res.json({ status: 200, message: `Contact details for ID ${id}`, data: contact });
-    } catch (err) {
-      next(err);
-    }
-  });
+  app.use(notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    console.error('Server error:', err);
-    res.status(500).json({ message: 'Something went wrong', error: err.message });
-  });
+  app.use(errorHandler);
 
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
+  app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   });
-
-  return app;
 };
